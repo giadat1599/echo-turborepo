a51497bb1a5bc66a33c3ab8d9606ca988325d794
Dat Gia Truong <datgiatruong@192.168.2.50>
2025-08-26 23:17:23 +0700

    feat: convex package

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,14 +1,15 @@
-import { Button } from '@workspace/ui/components/button';
-import { Input } from '@workspace/ui/components/input';
-import { add } from '@workspace/math/add';
+'use client';
+
+import { useQuery } from 'convex/react';
+import { api } from '@workspace/backend/_generated/api';
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello Turbo Widget</h1>
-        <Button size="sm">{add(2, 2)}</Button>
-        <Input />
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p>apps/widget</p>
+      <div className="max-w-sm w-full mx-auto">
+        {JSON.stringify(users, null, 2)}
       </div>
     </div>
   );
