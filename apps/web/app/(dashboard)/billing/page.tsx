165cd94083b849ce8c9dacf3301c2d4fdb3dde76
Dat Gia Truong <datgiatruong@Dats-MacBook-Pro-2.local>
2025-08-31 19:45:37 +0700

    feat: dashboard layout

---
+++ apps/web/app/(dashboard)/billing/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Billing</h1>
+    </div>
+  );
+}
--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,10 +1,5 @@
-import { AuthGuard } from '@/modules/auth/ui/components/auth-guard';
-import { OrganizationGuard } from '@/modules/auth/ui/components/organisation-guard';
+import { DashboardLayout } from '@/modules/dashboard/ui/layouts/dashboard-layout';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <AuthGuard>
-      <OrganizationGuard>{children}</OrganizationGuard>
-    </AuthGuard>
-  );
+  return <DashboardLayout>{children}</DashboardLayout>;
 }
